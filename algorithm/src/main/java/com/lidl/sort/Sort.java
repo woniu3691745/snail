9834e11bbb32feff18f022ece34d1084863dfaaa
lidl <3691745@qq.com>
2018-07-04 17:12:03 +0800

    add sort algorithm.

--- algorithm/src/main/java/com/lidl/sort/Sort.java
+++ algorithm/src/main/java/com/lidl/sort/Sort.java
@@ -18,16 +18,19 @@ public class Sort {
         System.out.println("排序前的数组为：" + Arrays.toString(array));
 //        bubbleSort(array);
 //        selectSort(array);
-        selectSort1(array);
+//        selectSort1(array);
 //        insertSort(array);
 //        insertSort1(array);
 //        shellSort(array);
 //        shellSort1(array);
 //        quickSort(array, 0, array.length - 1);
 //        heapSort(array, array.length);
+        heapSort(array);
 //        mergerSort(array, array.length);
         System.out.println("排序后的数组为：" + Arrays.toString(array));
     }
+
+    /*冒泡排序*/
 
     /**
      * 冒泡排序
@@ -55,6 +58,39 @@ public class Sort {
         }
     }
 
+    /**
+     * 一般不用。
+     * <p>
+     * 将序列中所有元素两两比较，将最大的放在最后面。
+     * 将剩余序列中所有元素两两比较，将最大的放在最后面。
+     * 重复第二步，直到只剩下一个数。
+     * <p>
+     * <p>
+     * 如何写成代码：
+     * <p>
+     * 设置循环次数。
+     * 设置开始比较的位数，和结束的位数。
+     * 两两比较，将最小的放到前面去。
+     * 重复2、3步，直到循环次数完毕。
+     *
+     * @param a 数组
+     */
+    private static void bubbleSort1(int[] a) {
+        int temp;
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a.length - i - 1; j++) {
+                if (a[j] > a[j + 1]) {
+                    temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+
+
+    /*选择排序*/
 
     /**
      * 选择排序
@@ -124,6 +160,9 @@ public class Sort {
     }
 
 
+
+    /*插入排序*/
+
     /**
      * 插入排序
      * （1）首先对数组的前两个数据进行从小到大的排序；
@@ -185,6 +224,8 @@ public class Sort {
         }
     }
 
+
+    /*希尔排序*/
 
     /**
      * shell(希尔排序)
@@ -253,6 +294,9 @@ public class Sort {
     }
 
 
+
+    /*快速排序*/
+
     /**
      * 快速排序
      * （1）首先设定一个分界值，通过该分界值将数组分成左右两部分；
@@ -268,13 +312,10 @@ public class Sort {
      * @param right 右侧第一个
      */
     private static void quickSort(int[] a, int left, int right) {
-
         int index, t;
         int rtemp, ltemp;
-
         ltemp = left;
         rtemp = right;
-
         index = a[(left + right) / 2];  // 任意数为中间位置
         while (ltemp < rtemp) {
             while (a[ltemp] < index) {  // 从左往右找到第一个大于key的数
@@ -291,7 +332,7 @@ public class Sort {
                 ++ltemp;
             }
         }
-        if (ltemp == rtemp) {   //
+        if (ltemp == rtemp) {
             ltemp++;
         }
         if (left < rtemp) { // 左侧
@@ -301,6 +342,45 @@ public class Sort {
             quickSort(a, rtemp + 1, right);
         }
     }
+
+    /**
+     * 要求时间最快时。
+     * <p>
+     * 选择第一个数为p，小于p的数放在左边，大于p的数放在右边。
+     * 递归的将p左边和右边的数都按照第一步进行，直到不能递归。
+     *
+     * @param numbers 数据
+     * @param start   开始位置
+     * @param end     结束位置
+     */
+    private static void quickSort1(int[] numbers, int start, int end) {
+        if (start < end) {
+            int base = numbers[start];      // 选定的基准值（第一个数值作为基准值）
+            int temp;                       // 记录临时中间值
+            int i = start, j = end;
+            do {
+                while ((numbers[i] < base) && (i < end))
+                    i++;
+                while ((numbers[j] > base) && (j > start))
+                    j--;
+                if (i <= j) {
+                    temp = numbers[i];
+                    numbers[i] = numbers[j];
+                    numbers[j] = temp;
+                    i++;
+                    j--;
+                }
+            } while (i <= j);
+            if (start < j)
+                quickSort1(numbers, start, j);
+            if (end > i)
+                quickSort1(numbers, i, end);
+        }
+    }
+
+
+
+    /*堆排序*/
 
     /**
      * 堆排序
@@ -384,6 +464,73 @@ public class Sort {
     }
 
     /**
+     * 对简单选择排序的优化。
+     * <p>
+     * 将序列构建成大顶堆。
+     * 将根节点与最后一个节点交换，然后断开最后一个节点。
+     * 重复第一、二步，直到所有节点断开。
+     * <p>
+     * 最后一个节点的父节点开始 >> i = (lastIndex - 1) / 2
+     * k节点的左子节点的索引 >> int biggerIndex = 2 * k + 1;
+     *
+     * @param a 数组
+     */
+    private static void heapSort(int[] a) {
+        System.out.println("开始排序");
+        int arrayLength = a.length;
+        // 循环建堆
+        for (int i = 0; i < arrayLength - 1; i++) {
+            // 建堆
+            // 对data数组从0到lastIndex建大顶堆
+            buildMaxHeap(a, arrayLength - 1 - i);
+            // 交换堆顶和最后一个元素，之后重新建堆
+            swap(a, 0, arrayLength - 1 - i);
+            System.out.println(Arrays.toString(a));
+        }
+    }
+
+    private static void swap(int[] data, int i, int j) {
+        int tmp = data[i];
+        data[i] = data[j];
+        data[j] = tmp;
+    }
+
+    private static void buildMaxHeap(int[] data, int lastIndex) {
+        // 从lastIndex处节点（最后一个节点）的父节点开始
+        for (int i = (lastIndex - 1) / 2; i >= 0; i--) {
+            // k保存正在判断的节点
+            int k = i;
+            // 如果当前k节点的子节点存在
+            while (k * 2 + 1 <= lastIndex) {
+                // k节点的左子节点的索引
+                int biggerIndex = 2 * k + 1;
+                // 如果biggerIndex小于lastIndex，即biggerIndex+1代表的k节点的右子节点存在
+                if (biggerIndex < lastIndex) {
+                    // 若果右子节点的值较大
+                    if (data[biggerIndex] < data[biggerIndex + 1]) {
+                        // biggerIndex总是记录较大子节点的索引
+                        biggerIndex++;
+                    }
+                }
+                // 如果k节点的值小于其较大的子节点的值
+                if (data[k] < data[biggerIndex]) {
+                    // 交换他们
+                    swap(data, k, biggerIndex);
+                    // 将biggerIndex赋予k，开始while循环的下一次循环，重新保证k节点的值大于其左右子节点的值
+                    k = biggerIndex;
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+
+
+
+
+    /*合并排序*/
+
+    /**
      * @param a   原始数组
      * @param b   新数组
      * @param n   原数组长度
@@ -427,8 +574,8 @@ public class Sort {
      * 合并排序
      * 合并排序算法就是将多个有序
      *
-     * @param a
-     * @param n
+     * @param a 数组
+     * @param n 数组长度
      */
     private static void mergerSort(int[] a, int n) {
         int h, count, len, f;
@@ -457,4 +604,58 @@ public class Sort {
             }
         }
     }
+
+    /**
+     * 速度仅次于快排，内存少的时候使用，可以进行并行计算的时候使用。
+     * <p>
+     * 选择相邻两个数组成一个有序序列。
+     * 选择相邻的两个有序序列组成一个有序序列。
+     * 重复第二步，直到全部组成一个有序序列。
+     *
+     * @param numbers 数据
+     * @param left    左侧
+     * @param right   右侧
+     */
+    public static void mergeSort1(int[] numbers, int left, int right) {
+        int t = 1;              // 每组元素个数
+        int size = right - left + 1;
+        while (t < size) {
+            int s = t;          // 本次循环每组元素个数
+            t = 2 * s;
+            int i = left;
+            while (i + (t - 1) < size) {
+                merge1(numbers, i, i + (s - 1), i + (t - 1));
+                i += t;
+            }
+            if (i + (s - 1) < right)
+                merge1(numbers, i, i + (s - 1), right);
+        }
+    }
+
+    private static void merge1(int[] data, int p, int q, int r) {
+        int[] B = new int[data.length];
+        int s = p;
+        int t = q + 1;
+        int k = p;
+        while (s <= q && t <= r) {
+            if (data[s] <= data[t]) {
+                B[k] = data[s];
+                s++;
+            } else {
+                B[k] = data[t];
+                t++;
+            }
+            k++;
+        }
+        if (s == q + 1)
+            B[k++] = data[t++];
+        else
+            B[k++] = data[s++];
+        int i = p;
+        while (i <= r) {
+            data[i] = B[i];
+            i++;
+        }
+    }
+
 }
