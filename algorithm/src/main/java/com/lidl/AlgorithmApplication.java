5d2c658286814b55a4f754c53f6a9489984440c8
lidongliang <3691745@qq.com>
2018-07-08 15:08:27 +0800

    add notes.

--- algorithm/src/main/java/com/lidl/AlgorithmApplication.java
+++ algorithm/src/main/java/com/lidl/AlgorithmApplication.java
@@ -3,6 +3,10 @@ package com.lidl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author lidongliang
+ * @date 2018/7/4 下午9:52
+ */
 @SpringBootApplication
 public class AlgorithmApplication {
 
