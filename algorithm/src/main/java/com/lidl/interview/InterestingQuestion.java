fbf8e0c595479860df9f08467bc3d36ff937c294
lidongliang <3691745@qq.com>
2018-07-09 07:10:17 +0800

    update interview method.

---
+++ algorithm/src/main/java/com/lidl/interview/InterestingQuestion.java
@@ -0,0 +1,89 @@
+package com.lidl.interview;
+
+/**
+ * 经典趣题
+ *
+ * @author lidongliang
+ * @date 2018/7/8 下午5:36
+ */
+public class InterestingQuestion {
+
+    /**
+     * 百钱买百鸡
+     * x+y+z=100
+     * 5x+3y+z/3=100
+     *
+     * @param m 钱
+     * @param n 鸡
+     */
+    private static void bjbj(int m, int n) {
+        int x, y, z;
+        for (x = 0; x <= n; x++) {
+            for (y = 0; y <= n; y++) {
+                z = n - x - y;
+                if (z > 0 && z % 3 == 0 && x * 5 + y * 3 + z / 3 == m) {
+                    System.out.printf("公鸡：%d只，母鸡：%d只，小鸡：%d只\n", x, y, z);
+                }
+            }
+        }
+    }
+
+    /**
+     * 五家共井
+     * len1*2+len2=len
+     * len2*3+len3=len
+     * len3*4+len4=len
+     * len4*5+len5=len
+     * len5*6+len1=len
+     * <p>
+     * 变形
+     * len1*2+len2=len2*3+len3=len3*4+len4=len4*5+len5=len5*6+len1
+     * len1=len2+len3/2
+     * len2=len3+len4/3
+     * len3=len4+len5/4
+     * len4=len5+len1/5
+     * 由此可知
+     * len3能被2整除，len3必为2的倍数；
+     * len4能被3整除，len4必为3的倍数；
+     * len5能被4整除，len5必为4的倍数；
+     * len1能被5整除，len1必为5的倍数；
+     *
+     * @param len1 甲
+     * @param len2 已
+     * @param len3 丙
+     * @param len4 丁
+     * @param len5 戊
+     * @param len  井深
+     */
+    private static void wjgj(int[] len1, int[] len2, int[] len3, int[] len4, int[] len5, int[] len) {
+        for (len5[0] = 4; ; len5[0] += 4) {
+            for (len1[0] = 5; ; len1[0] += 5) {
+                len4[0] = len5[0] + len1[0] / 5;
+                len3[0] = len4[0] + len5[0] / 4;
+                if (len3[0] % 2 != 0 || len4[0] % 3 != 0) {
+                    continue;
+                }
+                len2[0] = len3[0] + len4[0] / 3;
+                if (len2[0] + len3[0] / 2 < len1[0]) {
+                    break;
+                }
+                if (len2[0] + len3[0] / 2 == len1[0]) {
+                    len[0] = 2 * (len1[0]) + (len2[0]);
+                    return;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+//        InterestingQuestion.bjbj(100, 100);
+        int[] len1 = {0};
+        int[] len2 = {0};
+        int[] len3 = {0};
+        int[] len4 = {0};
+        int[] len5 = {0};
+        int[] len = {0};
+        InterestingQuestion.wjgj(len1, len2, len3, len4, len5, len);
+        System.out.printf("井深：%d\n", len[0]);
+    }
+}
--- algorithm/src/main/java/com/lidl/interview/Question.java
+++ algorithm/src/main/java/com/lidl/interview/Question.java
@@ -18,10 +18,84 @@ public class Question {
             if (n <= 0) {
                 System.out.println("error: n must > 0");
             }
-            if (0 == n % 2)
+            if (0 == n % 2) {
                 System.out.println((n / 2) * (-1));
-            else
+            } else {
                 System.out.println((n / 2) * (-1) + n);
+            }
         }
+    }
+
+    /**
+     * 洗扑克牌
+     * <p>
+     * 洗扑克牌的原理其实与乱数排列是相同的，都是将一组数字（例如1～N）打乱重新排列，
+     * 现要求按照花色来排列扑克牌。
+     */
+    private static void solitaire() {
+        final int N = 52;
+        // 扑克牌一共52张
+        int[] solitaire = new int[N + 1];
+        // 初始化整列
+        for (int i = 1; i <= N; i++) {
+            solitaire[i] = i;
+        }
+        // 洗牌
+        for (int i = 1; i <= N; i++) {
+            // 生成新的52张牌
+            int j = (int) (Math.random() * N);
+            if (j == 0) {
+                j = 1;
+            }
+            int mid = solitaire[i];
+            solitaire[i] = solitaire[j];
+            solitaire[j] = mid;
+        }
+        for (int i = 1; i <= N; i++) {
+            // 判断花色 52/13=4
+            switch ((solitaire[i] - 1) / 13) {
+                case 0:
+                    System.out.print("♠️");
+                    break;
+                case 1:
+                    System.out.print("♥️");
+                    break;
+                case 2:
+                    System.out.print("♦️️");
+                    break;
+                case 3:
+                    System.out.print("♣️️");
+                    break;
+                default:
+                    break;
+            }
+            // 扑克牌数字
+            int number = solitaire[i] % 13;
+            switch (number) {
+                case 0:
+                    System.out.print("K ");
+                    break;
+                case 1:
+                    System.out.print("A ");
+                    break;
+                case 12:
+                    System.out.print("Q ");
+                    break;
+                case 11:
+                    System.out.print("J ");
+                    break;
+                default:
+                    System.out.print(number + " ");
+                    break;
+            }
+            // 控制打印行数，每13个数据换一行
+            if (i % 13 == 0) {
+                System.out.println("");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Question.solitaire();
     }
 }
